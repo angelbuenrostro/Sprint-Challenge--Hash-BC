29f0ce76565676bbb6f0a323c61056f5e1640ed3
Angel <angelbuenrostro@outlook.com>
2019-09-06 11:55:52 -0400

    Completed all hashtable tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,26 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for ticket in tickets:
+        hash_table_insert(
+            hashtable,
+            ticket.source,
+            ticket.destination
+        )
 
+    location = hash_table_retrieve(hashtable, "NONE")
+
+    for i in range(0, length):
+        route[i] = location
+        location = hash_table_retrieve(hashtable, location)
+    
     return route
+
+tickets = [
+    Ticket("PIT", "ORD"),
+    Ticket("NONE", "PIT"),
+    Ticket("ORD", "NONE")
+]
+
+route = reconstruct_trip(tickets, 3)
+print(route)
